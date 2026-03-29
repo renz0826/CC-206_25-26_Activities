48f4a9995ab66649935384d42d2c8717df6e1ed6
renz0826 <brenzbedonia@gmail.com>
2026-03-29 13:00:52 +0800

    feat: create Pet class

---
+++ CS 2B/RENZ_CLYD_BEDONIA/BEDONIA-ACTIVITY_5/Pet.dart
@@ -0,0 +1,63 @@
+// Pet Class
+
+// import Animal class
+import 'Animal.dart';
+
+class Pet extends Animal {
+  String? nickname;
+  int kindnessValue;
+
+  String get displayName => nickname ?? name;
+
+  // Pet constructors
+
+  // Constructor excluding pet nickname
+  Pet(super.name, super.kingdom, super.dob, super.numLegs, this.kindnessValue);
+
+  // Constructor including pet nickname
+  Pet.requiringNickName(
+    super.name,
+    super.kingdom,
+    super.dob,
+    super.numLegs,
+    this.nickname,
+  ) : kindnessValue = 50;
+
+  // Pet methods
+
+  // Function for kicking pet
+  void kick(int value) {
+    kindnessValue -= value;
+    print(
+      "Oh no! Poor $displayName was kicked.\nKindness Value: $kindnessValue\n",
+    );
+  }
+
+  // Function for petting pet
+  void pet(int value) {
+    if (kindnessValue > 0) {
+      kindnessValue += value;
+      print(
+        "Awww! $displayName has been petted.\nKindness Value: $kindnessValue\n",
+      );
+    } else {
+      print(
+        "$displayName snarled at you, you cannot pet it.\nKindness Value: $kindnessValue\n",
+      );
+    }
+  }
+
+  // Function for playing with pet
+  void play(int value) {
+    if (kindnessValue > 0) {
+      kindnessValue += value;
+      print(
+        "Awesome! You and $displayName had a great time playing.\nKindness Value: $kindnessValue\n",
+      );
+    } else {
+      print(
+        "$displayName does not want to play with you.\nKindness Value: $kindnessValue\n",
+      );
+    }
+  }
+}
