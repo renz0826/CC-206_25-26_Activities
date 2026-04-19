45747f5e3bfff796fdfb59cd606f7e0267eefe96
renz0826 <brenzbedonia@gmail.com>
2026-04-19 22:28:04 +0800

    feat: create pages for profile and pet

---
+++ CS 2B/RENZ_CLYD_BEDONIA/BEDONIA-ACTIVITY_6/lib/pages/pet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Pet extends StatelessWidget {
+  const Pet({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: Container(child: Center(child: Text("Pet"))),
+  );
+}
