1b864fac01ec780e07e7f89571ef9ec17a02fbbc
renz0826 <brenzbedonia@gmail.com>
2026-04-20 08:01:20 +0800

    feat: create app theme

---
+++ CS 2B/RENZ_CLYD_BEDONIA/BEDONIA-ACTIVITY_6/lib/style/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static const Color appGreen = Color.fromARGB(255, 28, 139, 0);
+
+  static ThemeData get lightTheme {
+    return ThemeData(
+      // Color Theme
+      colorScheme: ColorScheme.fromSeed(seedColor: appGreen),
+      scaffoldBackgroundColor: Colors.white,
+
+      // Text Theme
+      textTheme: TextTheme(
+        headlineMedium: GoogleFonts.geom(fontSize: 28),
+        titleLarge: GoogleFonts.geom(fontSize: 22),
+        bodyMedium: GoogleFonts.roboto(fontSize: 16),
+        bodySmall: GoogleFonts.roboto(fontSize: 14),
+        labelLarge: GoogleFonts.roboto(fontSize: 14),
+      ),
+
+  }
+}
