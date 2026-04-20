8ae2f9316cf03f3397cec6dd4722d07b964702c0
renz0826 <brenzbedonia@gmail.com>
2026-04-20 08:47:24 +0800

    feat: create profile header

--- CS 2B/RENZ_CLYD_BEDONIA/BEDONIA-ACTIVITY_6/lib/pages/profile.dart
+++ CS 2B/RENZ_CLYD_BEDONIA/BEDONIA-ACTIVITY_6/lib/pages/profile.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:simpe_flutter_application/style/theme.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-    body: Container(child: Center(child: Text("Hello World"))),
-  );
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Column(
+        children: <Widget>[
+          const SizedBox(height: 32),
+          Text(
+            "My Profile",
+            style: Theme.of(
+              context,
+            ).textTheme.headlineMedium?.copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 32),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 160,
+                height: 160,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(500),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/profile_picture.jpg'),
+                  ),
+                  border: Border.all(color: AppTheme.appGreen, width: 2),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Renz Clyd D. Bedonia",
+                      style: Theme.of(context).textTheme.headlineMedium
+                          ?.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "BSCS 2-B",
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 32),
+        ],
+      ),
+    );
+  }
 }
